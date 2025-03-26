380d27c656b83052540e9189df6f8d208ede31ab
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-26 21:52:42 +0000

    created controller actions for all pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def home
+    render({ template: "dice_templates/home"})
+  end
+
+  def two_six
+
+    render({ template: "dice_templates/2d6"})
+  end
+
+  def two_ten
+    
+    render({ template: "dice_templates/2d10"})
+  end
+
+  def one_twenty
+    
+    render({ template: "dice_templates/1d20"})
+  end
+
+  def five_four
+    
+    render({ template: "dice_templates/5_4"})
+  end
+end
