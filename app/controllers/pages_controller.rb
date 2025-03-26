9718d295f03f412ee8b0d6de34c9d251b27e0f2d
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-26 23:54:04 +0000

    assigned methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,26 @@ class PagesController < ApplicationController
   end
 
   def two_six
+    @dice_roll = [rand(1..6), rand(1..6)]
+    
 
     render({ template: "dice/two/six"})
   end
 
   def two_ten
+    @dice_roll = [rand(1..10), rand(1..10)]
     
     render({ template: "dice/two/ten"})
   end
 
   def one_twenty
+    @dice_roll = [rand(1..20)]
     
     render({ template: "dice/one/twenty"})
   end
 
   def five_four
+    @dice_roll = [rand(1..4), rand(1..4), rand(1..4), rand(1..4), rand(1..4)]
     
     render({ template: "dice/five/four"})
   end
