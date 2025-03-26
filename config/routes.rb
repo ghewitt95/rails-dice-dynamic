8182e3a568efff8aa7d46caba8e15a300edade0e
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-26 21:39:32 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "pages", :action => "home")
+
+  get("2d6", {:controller => "pages", :action => "two_six") 
+
+  get("2d10", {:controller => "pages", :action => "two_ten")
+
+  get("1d20", {:controller => "pages", :action => "one_twenty")
+
+  get("5d4", {:controller => "pages", :action => "five_four")
 
 end
