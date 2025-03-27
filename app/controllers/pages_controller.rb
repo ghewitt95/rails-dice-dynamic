7ccf5b7a091752f1c3e846a873a822a2d831b6fe
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-27 20:35:28 +0000

    made routes dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,28 +3,13 @@ class PagesController < ApplicationController
     render({ template: "dice/home"})
   end
 
-  def two_six
-    @dice_roll = [rand(1..6), rand(1..6)]
-    
+  def roll_dice
+    @num_dice = params[:num].to_i
+    @num_sides = params[:sides].to_i
+    @dice_rolls = Array.new(@num_dice) { rand(1..@num_sides) }
 
-    render({ template: "dice/two/six"})
-  end
-
-  def two_ten
-    @dice_roll = [rand(1..10), rand(1..10)]
-    
-    render({ template: "dice/two/ten"})
-  end
-
-  def one_twenty
-    @dice_roll = [rand(1..20)]
-    
-    render({ template: "dice/one/twenty"})
-  end
-
-  def five_four
-    @dice_roll = [rand(1..4), rand(1..4), rand(1..4), rand(1..4), rand(1..4)]
-    
-    render({ template: "dice/five/four"})
+    render({ template: "dice/roll" })
   end
 end
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   get("/", {:controller => "pages", :action => "home"})
-
-  get("two/six", {:controller => "pages", :action => "two_six"})
-
-  get("two/ten", {:controller => "pages", :action => "two_ten"})
-
-  get("one/twenty", {:controller => "pages", :action => "one_twenty"})
-
-  get("five/four", {:controller => "pages", :action => "five_four"})
+  
+  get("/dice/:num/:sides", { controller: "pages", action: "roll_dice" })
 
 end
-
-
